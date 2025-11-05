ce9a5b4713969018990b6eb212a050025325d53d
sakamata0 <iskanderboughnimi@gmail.com>
2025-11-05 21:06:44 +0100

    resolving conflict of app.html/css/ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardPanel } from './shared/components/card-panel/card-panel';
 import { FlurrHeaderComponent } from './shared/components/flurr-header/flurr-header';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
+    CardPanel,
     FlurrHeaderComponent
   ],
   templateUrl: './app.html',
---
+++ src/app/shared/components/card-panel/card-panel.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+import { ItemPanelInfo } from '../../model/item-panel.type';
+import { NgFor, NgIf } from '@angular/common';
+import { ItemPanel } from '../item-panel/item-panel';
+
+@Component({
+  selector: 'app-card-panel',
+  imports: [NgIf, NgFor, ItemPanel],
+  templateUrl: './card-panel.html',
+  styleUrl: './card-panel.css'
+})
+export class CardPanel {
+  title = input("Title");
+  noItemsMessage = input("No Items to show.");
+  items = input<Array<ItemPanelInfo>>([
+    {id: '1', title: 'Mohamed Houcine', imageUrl: 'assets/images/hama.png', withSubtitle: true, subtitle: 'liked your Flurr!', subtitleOnSameLevel: true, withIcon: true, iconUrl: 'assets/icons/like.png', withButton: false},
+    {id: '2', title: 'Mohamed Houcine', imageUrl: 'assets/images/hama.png', withSubtitle: true, subtitle: 'commented on your Flurr!', subtitleOnSameLevel: true, withIcon: true, iconUrl: 'assets/icons/comment.png', withButton: false},
+    {id: '3', title: 'Skander Boughnimi', imageUrl: 'assets/images/skander.png', withSubtitle: true, subtitle: '4.7k Followers', subtitleOnSameLevel: false, withIcon: false, withButton: true, buttonText: 'Follow', buttonAction: () => { console.log('Button clicked'); } }
+  ]);
+}
