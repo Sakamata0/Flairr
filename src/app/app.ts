fc4175a5b03bc61b71681c3e272f508c6e9e619b
Ismail Mechkene <imechkene@gmail.com>
2025-11-05 16:56:01 +0100

    3malt el header ama naçes el messages wl notifications icon mazel na9es chwya modifications

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlurrHeaderComponent } from './shared/components/flurr-header/flurr-header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FlurrHeaderComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Flairr');
+     
 }
