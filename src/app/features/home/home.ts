393c9ccbcce028a7e61880b4742ee9d08bdc2ba0
sakamata0 <iskanderboughnimi@gmail.com>
2025-11-06 19:25:43 +0100

    sala7t border radius mta3 dialog (mta3 aman)

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { FlurrCreationCard } from '../../shared/components/flurr-creation-card/flurr-creation-card';
 import { CardPanel } from '../../shared/components/card-panel/card-panel';
 import { FlurrHeaderComponent } from '../../shared/components/flurr-header/flurr-header';
+import { ProfileHeader } from "../../shared/components/profile/profile-header/profile-header";
 
 @Component({
   selector: 'app-home',
   imports: [
     FlurrCreationCard,
-    CardPanel
-  ],
+    CardPanel,
+    ProfileHeader
+],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
