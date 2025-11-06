a01fa9ea1d2569bd037952999c2e909df6f92632
Ismail Mechkene <imechkene@gmail.com>
2025-11-06 19:34:28 +0100

    initializing mini profile card

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FlurrCreationCard } from '../../shared/components/flurr-creation-card/flurr-creation-card';
 import { CardPanel } from '../../shared/components/card-panel/card-panel';
-import { FlurrHeaderComponent } from '../../shared/components/flurr-header/flurr-header';
+import { MiniProfileCard } from '../../shared/components/mini-profile-card/mini-profile-card';
 
 @Component({
   selector: 'app-home',
   imports: [
     FlurrCreationCard,
-    CardPanel
+    CardPanel,
+    MiniProfileCard
   ],
   templateUrl: './home.html',
   styleUrl: './home.css'
