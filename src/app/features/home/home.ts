6f6eea9295d732210262658e2d38c93bba06e0b5
amenallah53 <amenkalai53@gmail.com>
2025-11-07 17:51:36 +0100

    modifications: responsive dialogs

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FlurrCreationCard } from '../../shared/components/flurr-creation-card/flurr-creation-card';
 import { CardPanel } from '../../shared/components/card-panel/card-panel';
-import { FlurrHeaderComponent } from '../../shared/components/flurr-header/flurr-header';
 
 @Component({
   selector: 'app-home',
@@ -13,5 +12,5 @@ import { FlurrHeaderComponent } from '../../shared/components/flurr-header/flurr
   styleUrl: './home.css'
 })
 export class Home {
-
+  
 }
