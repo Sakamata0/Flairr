1f0da36777c1b6e6140f354c44edab4ffcccefa5
Ismail Mechkene <imechkene@gmail.com>
2025-12-13 23:40:10 +0100

    feat(friends): add follow request handling, suggestion removal, and custom confirmation dialog

--- frontend/src/app/shared/components/friend-request/friend-request.ts
+++ frontend/src/app/shared/components/friend-request/friend-request.ts
@@ -1,34 +1,37 @@
 import { Component, input, Output, EventEmitter } from '@angular/core';
 import { FriendsProfile } from '../../model/friends-profile.type';
 import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-friend-request',
-  imports: [NgIf],
   standalone: true,
+  imports: [NgIf],
   templateUrl: './friend-request.html',
   styleUrl: './friend-request.css'
 })
 export class FriendRequest {
+
   info = input.required<FriendsProfile>();
   variab = input.required<number>();
-
-  getHandle(name: string): string {
-    return '@' + name.replace(' ', '_').toLowerCase();
-  }
-
-  onImageError(event: Event) {
-    const imgElement = event.target as HTMLImageElement;
-    imgElement.src = 'assets/images/default-profile-picture.png';
-  }
-  onBannerError(event: Event) {
-    const imgElement = event.target as HTMLImageElement;
-    imgElement.src = 'assets/images/default-banner-image.png';
-  }
 
   @Output() accept = new EventEmitter<void>();
   @Output() follow = new EventEmitter<void>();
   @Output() remove = new EventEmitter<void>();
 
+  constructor(private router: Router) {}
 
+  goToProfile() {
+    this.router.navigate(['/profile', this.info().id]);
+  }
+
+  onImageError(event: Event) {
+    (event.target as HTMLImageElement).src =
+      'assets/images/default-profile-picture.png';
+  }
+
+  onBannerError(event: Event) {
+    (event.target as HTMLImageElement).src =
+      'assets/images/default-banner-image.png';
+  }
 }
