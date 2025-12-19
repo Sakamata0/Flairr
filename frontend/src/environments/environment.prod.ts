53a000bf78617c739ad9be80b97c32b23a86264c
Ismail Mechkene <153388041+IsmailMechkene@users.noreply.github.com>
2025-12-19 15:04:29 +0100

    Update Supabase configuration in production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'SUPABASE_URL',
-  supabaseAnonKey: 'SUPABASE_ANON_KEY'
+  supabaseUrl: 'https://dmqumeubogwpbhmckibx.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcXVtZXVib2d3cGJobWNraWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0Mjk1NTcsImV4cCI6MjA4MDAwNTU1N30.msU8aFc_dw2uR23Yi6HAV8phCIk8pcZ2-e5li5RKjls'
 };
