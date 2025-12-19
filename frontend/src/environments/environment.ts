ae6c52959bb49d7bf012d210b7359f378574d3c4
Ismail Mechkene <imechkene@gmail.com>
2025-12-19 15:10:01 +0100

    comm

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://dmqumeubogwpbhmckibx.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcXVtZXVib2d3cGJobWNraWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0Mjk1NTcsImV4cCI6MjA4MDAwNTU1N30.msU8aFc_dw2uR23Yi6HAV8phCIk8pcZ2-e5li5RKjls'
 };
